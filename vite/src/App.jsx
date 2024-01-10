d3a80f476dbd078f335e32900be65808dcd224b6
Manvi <nainachaudharydehradun@gmail.com>
2024-01-10 10:08:00 +0530

    updating

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -3,13 +3,38 @@
 import { useState } from "react"
 function App() {
   let [counter,setCounter] = useState(15)
+  // const [counter,setCounter] = useState(15)
+  // Is an array so doesn't make any difference 
   // let counter = 15
 
   let addVal =()=>{
-    setCounter(counter+1 )
+    if(counter<20){
+      setCounter(counter+1 )
+<<<<<<< HEAD
+
+      //Question
+      // setCounter(counter+1 )
+      // setCounter(counter+1 )
+      // setCounter(counter+1 )
+      // setCounter(counter+1 )
+      // Will only update value once because of fibre concept which will bundle all thes calls.
+
+      //Instead use approach
+      //setCounter((prevCounter)=>PrevCounter+1)
+      //setCounter((prevCounter)=>PrevCounter+1)
+      //setCounter((prevCounter)=>PrevCounter+1)
+      //setCounter((prevCounter)=>PrevCounter+1)
+      //Now it is updating value previous state by 2 everytime  
+=======
+>>>>>>> e0c5fc8fb48cacb263069b817e499791c597f77a
+    }
+    
   }
   let decVal =()=>{
-    setCounter(counter-1)
+    if(counter>0){
+      setCounter(counter-1)
+    }
+    
   }
   return (
     
