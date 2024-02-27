b821a80c9866ba2f92c7c1bd08df5f91873ca396
Manvi <nainachaudharydehradun@gmail.com>
2024-02-27 21:24:06 +0530

    adding files

--- context2/src/assets/components/Item.jsx
+++ context2/src/assets/components/Item.jsx
@@ -5,6 +5,11 @@ import { useContext } from 'react'
 
 function Item(props) {
     const card = useContext(CardContext);
+
+    const removeItem = () =>{
+      const updatedItem = card.items.filter((item)=> item.name !==props.name);
+      card.setItems(updatedItem);
+    }
     console.log(card);
   return (
     <div className='item-card'>
@@ -12,7 +17,8 @@ function Item(props) {
         <h4>Item-name : {props.name}</h4>
         <h5>Price:${props.price}</h5>
         <button onClick={()=> card.setItems([...card.items,{name:props.name,price:props.price}])}>Add to cart</button>
-        {/* Make remove button  */}
+        
+        <button onClick={removeItem}>Remove</button>
     </div>
     </div>
     
